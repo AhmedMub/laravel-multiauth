2a8af87857ddb6e38778dbfcecec9605b6d1fe14
AhmedMub <mada.mub@gmail.com>
2022-01-16 00:33:41 +0300

    follow till video 101

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//for Admin dashboard
+Route::prefix('admin')->middleware(['admin:admin'])->name('admin.')->group(function () {
+
+    Route::get('/login', [AdminController::class, 'adminLogin'])->name('login');
+
+    Route::post('/login', [AdminController::class, 'store'])->name('store');
+});
+
+//admin middleware
+Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+
+//user middleware
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
